14d54ac1740ee76248884b128162061fea08cf8e
Igor Konnov <57567427+igorkonnov@users.noreply.github.com>
2021-03-09 20:19:36 +0300

    Delete grath.py

--- pages/grath.py
+++
@@ -1,8 +0,0 @@
-import dash_html_components as html
-#from utils import Header, make_dash_table
-import pandas as pd
-import pathlib
-
-# get relative data folder
-PATH = pathlib.Path(__file__).parent
-DATA_PATH = PATH.joinpath("../data").resolve()
